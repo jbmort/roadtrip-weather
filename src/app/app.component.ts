231d333c4f9db02942296d910817081455ddf646
Jacob Mortensen <jake.b.mortensen@gmail.com>
2025-01-15 10:30:49 -0600

    add search form and basic functions for getting values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OpenMapComponent } from "./open-map/open-map.component";
+import { LocationSearchComponent } from "./location-search/location-search.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OpenMapComponent],
+  imports: [RouterOutlet, OpenMapComponent, LocationSearchComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'roadtrip';
--- src/app/location-search/location-search.component.ts
+++ src/app/location-search/location-search.component.ts
@@ -4,19 +4,31 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-location-search',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,],
   templateUrl: './location-search.component.html',
-  styleUrl: './location-search.component.css'
+  styleUrl: './location-search.component.css',
 })
 export class LocationSearchComponent {
 
   routeForm = new FormGroup({
-    whereFrom: new FormControl(''),
-    whereTo: new FormControl('')})
+    start: new FormControl(''),
+    ending: new FormControl('')})
 
 
   onSearch(){
+    console.log(this.routeForm.value)
+  }
 
+  updateStart(){
+    if(this.routeForm.value.start?.length! > 3){
+    console.log(this.routeForm.value.start)
+    }
+  }
+
+  updateEnd(){
+    if(this.routeForm.value.ending?.length! > 3){
+    console.log(this.routeForm.value.ending)
+    }
   }
 
 }
