e268b2686d9fff04b174fad9a95ab6d978b0e5ee
Jacob Mortensen <jake.b.mortensen@gmail.com>
2025-03-25 10:06:08 -0500

    add basic trip summary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { OpenMapComponent } from "./open-map/open-map.component";
 import { LocationSearchComponent } from "./location-search/location-search.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { TripInfoComponent } from './trip-info/trip-info.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OpenMapComponent, LocationSearchComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, OpenMapComponent, LocationSearchComponent, HeaderComponent, FooterComponent, TripInfoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/trip-info/trip-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip-info.component.html',
+  styleUrl: './trip-info.component.css'
+})
+export class TripInfoComponent {
+
+}
